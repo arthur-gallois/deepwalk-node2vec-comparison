4163b8c15eca5271f92faca358930ae969efda55
arthur-gallois <arthur.gallois@protonmail.com>
2024-03-24 12:32:26 +0100

    Added node2vec implementation, cleaned deepWalk.ipynb, added deepWalk_experiments and node2vec_experiments

--- node_embeddings/model.py
+++ node_embeddings/model.py
@@ -2,7 +2,7 @@ from tqdm import tqdm
 import torch
 import numpy as np
 import random
-from node_embeddings.random_walk import generate_windows, gen_batch_random_walk, get_windows_dotproduct, gen_batch_negative_samples 
+from .random_walk import generate_windows, gen_batch_random_walk, get_windows_dotproduct, gen_batch_negative_samples,gen_batch_biaised_random_walk
 
 eps = 1e-15
 
@@ -56,4 +56,50 @@ def deepWalk(graph, walks_per_vertex, walk_length, window_size, embedding_size, 
             optimizer.step()
             optimizer.zero_grad()  
 
+    return embedding, loss_history
+
+def node2vec(graph, walks_per_vertex, walk_length, window_size, embedding_size, num_neg, lr, epochs, batch_size, p = 5, q = 5):
+    number_of_nodes = graph.number_of_nodes()
+    
+    embedding = (torch.randn(size=(number_of_nodes, embedding_size)) ).detach()
+    embedding.requires_grad = True
+    optimizer = torch.optim.Adam([embedding], lr=lr)
+    loss_history = {'pos': [], 'neg': [], 'total': []}
+    neighbors_dict = {node: list(graph.neighbors(node)) for node in graph.nodes}
+
+    for _ in range(epochs):
+        nodes = torch.tensor(list(graph.nodes), dtype=int)
+        random.shuffle(nodes)
+        node_loader = generate_batches(nodes, batch_size)
+        n_batches = int(number_of_nodes / batch_size)
+        for n in tqdm(node_loader, total=n_batches):
+            random_walk = gen_batch_biaised_random_walk(graph, n, walk_length, walks_per_vertex, p, q, neighbors_dict)
+            num_windows = walk_length + 1 - window_size
+
+            # Positive Sampling
+            # each row of windows is one window, we have B = walks_per_vertex*num_windows windows
+            windows = generate_windows(random_walk, window_size)
+            batch_dotproduct = get_windows_dotproduct(windows, embedding)
+            # takes the sigmoid of the dot product to get probability, then
+            # takes the loglik and average through all elements
+            pos_loss = -torch.log(torch.sigmoid(batch_dotproduct)+eps).mean()
+            # Negative Sampling
+            negative_samples = gen_batch_negative_samples(
+                amount=num_neg*walks_per_vertex, 
+                length=walk_length, 
+                initial_nodes=n, 
+                number_of_nodes=number_of_nodes
+            )
+            windows = generate_windows(negative_samples, window_size)
+            batch_dotproduct = get_windows_dotproduct(windows, embedding)
+            neg_loss = -torch.log(1-torch.sigmoid(batch_dotproduct)+eps).mean()
+
+            loss = pos_loss + neg_loss
+            # Optimization
+            loss.backward()
+            loss_history['total'].append(loss.detach().numpy())
+            loss_history['pos'].append(pos_loss.detach().numpy())
+            loss_history['neg'].append(neg_loss.detach().numpy())
+            optimizer.step()
+            optimizer.zero_grad()
     return embedding, loss_history
--- node_embeddings/random_walk.py
+++ node_embeddings/random_walk.py
@@ -65,3 +65,49 @@ def gen_batch_negative_samples(amount, length, initial_nodes, number_of_nodes, d
         N = length-1
         negative_samples[:, 1:] = torch.multinomial(distribution, M*N, replacement=True).view(M, N)
     return negative_samples
+
+def gen_biaised_random_walk_tensor(graph, start_node, walk_length, num_walks, p, q , neighbors_dict):
+    walks = torch.zeros((num_walks, walk_length), dtype=int)
+    walks[:, 0] = start_node
+
+    for walk_index in range(num_walks):
+        current_node = start_node
+        for step in range(walk_length):
+            walks[walk_index, step] = current_node
+            if step > 0:
+                prev_node = int(walks[walk_index, step - 1])
+                current_node = get_next_node(graph,prev_node,current_node,p,q)
+            else:
+                current_node = np.random.choice(list(graph.neighbors(current_node)))
+            
+    
+    return walks
+
+def get_next_node(graph,t,v, p, q):
+    v_neighbors = set(graph.neighbors(v))
+    t_neighbors = set(graph.neighbors(t))
+    t_set = set([t])
+
+    vt_neighbors = v_neighbors & t_neighbors
+    only_v_neighbors = v_neighbors - t_neighbors - t_set
+
+    allsets = [vt_neighbors,only_v_neighbors,t_set]
+
+    vt_weights = 1 * len(vt_neighbors)
+    only_v_weights = 1/q * len(only_v_neighbors)
+    t_weight = 1/p
+
+    prob_vector = np.array((vt_weights,only_v_weights,t_weight))
+    prob_vector = prob_vector / np.sum(prob_vector)
+
+    chosen_set = np.random.choice(allsets,p=prob_vector)
+    next_node = np.random.choice(list(chosen_set))
+    return next_node
+
+def gen_batch_biaised_random_walk(graph, initial_nodes, length, num_walks, p, q, neighbors_dict):
+    n_nodes = initial_nodes.shape[0]
+    walk = torch.zeros((num_walks*n_nodes, length), dtype=int)
+    for i, n in enumerate(initial_nodes):
+        n = n.item()
+        walk[num_walks*i:num_walks*(i+1)] = gen_biaised_random_walk_tensor(graph, n, length, num_walks, p, q,neighbors_dict)
+    return walk

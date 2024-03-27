d49a842f8b36bd3d6eca7cb72e16167f97858f77
alexandre <alexandremr01@gmail.com>
2024-03-27 17:50:11 +0100

    fixes in split experiment

--- node_embeddings/experiments/split_experiment.py
+++ node_embeddings/experiments/split_experiment.py
@@ -11,10 +11,11 @@ from node_embeddings.model import deepWalk
 from node_embeddings.dataset import blogcatalog
 from node_embeddings.experiments.base import save_figure, init
 from sklearn.model_selection import train_test_split
+from node_embeddings.dataset import BlogCatalogDataset
 
 import random
 
-EXPERIMENT_NAME = 'split_experiment'
+EXPERIMENT_NAME = 'split_experiment3'
 EXPERIMENT_OUTPUT_PATH = join('output', EXPERIMENT_NAME)
 SEED=1234
 
@@ -22,7 +23,7 @@ def evaluate(embedding, labels, labeled_portion):
     X = embedding.detach().numpy()
     y = labels
     
-    X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=labeled_portion)
+    X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=(1-labeled_portion))
 
     clf = LogisticRegression(random_state=0, multi_class='ovr').fit(X_train, y_train)
     y_hat = clf.predict(X_test)
@@ -33,7 +34,7 @@ def evaluate(embedding, labels, labeled_portion):
 
 if __name__ == '__main__':
     init(path=EXPERIMENT_OUTPUT_PATH, seed=SEED)
-    bc_dataset = blogcatalog.load_data()
+    bc_dataset = BlogCatalogDataset().load()
 
     embedding, loss_history = deepWalk(
         graph=bc_dataset['graph'],  
@@ -41,14 +42,15 @@ if __name__ == '__main__':
         walk_length=40, 
         window_size=10,  
         embedding_size=128,
-        num_neg=5,
+        num_neg=10,
         lr=1e-2,
-        epochs=2,
+        epochs=6,
         batch_size=50
     )
     torch.save(embedding, join(EXPERIMENT_OUTPUT_PATH, 'embedding.pt'))
     with open(join(EXPERIMENT_OUTPUT_PATH, 'loss_history.pickle'), 'wb') as handle:
         pickle.dump(bc_dataset, handle)
+    embedding = torch.load(join(EXPERIMENT_OUTPUT_PATH, 'embedding.pt'))
 
     cumsum_vec = np.cumsum(np.insert(loss_history['total'], 0, 0)) 
     window_width = 10
@@ -59,7 +61,7 @@ if __name__ == '__main__':
     labeled_portions = np.linspace(0.1, 0.9, 9)
     for labeled_portion in labeled_portions:
         print('Labeled portion: ', labeled_portion)
-        microf1, macrof1 = evaluate(embedding, bc_dataset['labels'], labeled_portion=0.5)
+        microf1, macrof1 = evaluate(embedding, bc_dataset['labels'], labeled_portion=labeled_portion)
         print(f'Micro F1:  {microf1:.2%}')
         print(f'Macro F1: {macrof1:.2%}')
         print()
